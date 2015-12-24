0a3e1a92be7f3edc2a1e1776b24e93e4fdb720a4
Pedro Mendes da Silva <tagido@gmail.com>
2015-12-24 14:00:05 +0000

    sockets_common.h

---
+++ src/sockets_common.h
@@ -0,0 +1,28 @@
+/*
+ * sockets_common.h - support for cross-platform sockets
+ *
+ * Copyright (C) 2015 Pedro Mendes da Silva
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#ifndef SOCKETS_COMMON_H
+#define SOCKETS_COMMON_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "vmci_sockets.h"
+
+#endif
